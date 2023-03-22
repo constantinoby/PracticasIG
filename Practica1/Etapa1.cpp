97036390a2a0bde6969d6d90b77ecee987facbc3
Constantino Byelov <csbyelov@gmail.com>
2023-03-22 15:56:35 +0100

    Add project files.

---
+++ Practica1/Etapa1.cpp
@@ -0,0 +1,115 @@
+﻿// Etapa1.cpp
+// Fichero principal 
+////////////////////////////////////////////////////
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+const int W_WIDTH = 500; // Tama�o incial de la ventana
+const int W_HEIGHT = 500;
+GLfloat fAngulo; // Variable que indica el �ngulo de rotaci�n de los ejes. 
+
+// Funci�n que visualiza la escena OpenGL
+void Display(void)
+{
+	// Borramos la escena
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glPushMatrix();
+	// Rotamos las proximas primitivas
+	glRotatef(fAngulo, 0.0f, 0.0f, 1.0f);
+	// Creamos a continuaci�n dibujamos los tres poligonos
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(1.0f, 0.0f, 0.0f);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(-0.5f, 0.866f, 0.0f);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(1.0f, 0.0f, 0.0f);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.5f, -0.866f, 0.0f);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glColor3f(0.0f, 1.0f, 1.0f);
+	glVertex3f(-0.5f, 0.866f, 0.0f);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.5f, -0.866f, 0.0f);
+	glEnd();
+	glPopMatrix();
+
+	glFlush();
+}
+
+// Función que se ejecuta cuando se redimensiona la ventana
+void reshape(int width, int height) {
+	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	double aspect = (double)width / (double)height;
+	if (aspect > 1.0) {
+		glOrtho(-aspect, aspect, -1.0, 1.0, -1.0, 1.0);
+	}
+	else {
+		glOrtho(-1.0, 1.0, -1.0 / aspect, 1.0 / aspect, -1.0, 1.0);
+	}
+	glMatrixMode(GL_MODELVIEW);
+}
+
+
+
+
+// Funci�n que se ejecuta cuando el sistema no esta ocupado
+void Idle(void)
+{
+	// Incrementamos el �ngulo
+	fAngulo += 0.3f;
+	// Si es mayor que dos pi la decrementamos
+	if (fAngulo > 360)
+		fAngulo -= 360;
+	// Indicamos que es necesario repintar la pantalla
+	glutPostRedisplay();
+	glutSwapBuffers();
+
+}
+
+// Funci�n principal
+int main(int argc, char** argv)
+{
+	// Inicializamos la librer�a GLUT
+	glutInit(&argc, argv);
+
+	// Indicamos como ha de ser la nueva ventana
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(W_WIDTH, W_HEIGHT);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+	
+
+	// Creamos la nueva ventana
+	glutCreateWindow("Mi primera Ventana");
+
+	// Indicamos cuales son las funciones de redibujado e idle
+	glutDisplayFunc(Display);
+	glutIdleFunc(Idle);
+	
+	glutReshapeFunc(reshape);
+	
+
+
+	// El color de fondo ser� el negro (RGBA, RGB + Alpha channel)
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glOrtho(-1.0, 1.0f, -1.0, 1.0f, -1.0, 1.0f);
+
+	
+	// Comienza la ejecuci�n del core de GLUT
+	glutMainLoop();
+	return 0;
+}
